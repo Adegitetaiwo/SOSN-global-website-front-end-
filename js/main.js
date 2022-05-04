136abbc7722f5ed2d35533527372720202f84bf5
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-05-04 06:20:23 +0100

    adding sharing feature

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+const clickToCopyBtn = document.getElementById("clickToCopy");
 function myFunction() {
   /* Get the text field */
-  var copyText = document.getElementById("myInput");
+  const copyText = document.getElementById("myInput");
 
    /* Copy the text inside the text field */
     navigator.clipboard.writeText(copyText.innerText);
-
+    clickToCopyBtn.innerText = "Text Copied to clipboard!"
   /* Alert the copied text */
-  alert("Text Copied to clipboard");
+    setTimeout((e) => {
+        clickToCopyBtn.innerText = "Click to Copy text"
+    }, 1000)
 }
