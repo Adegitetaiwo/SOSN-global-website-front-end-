63220149ce93d3ec3145d67403fa159a1c32a5f8
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-05-04 05:08:58 +0100

    adding sharing feature

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
-const shareButton = document.querySelector('.share-button');
-const shareDialog = document.querySelector('.share-dialog');
-const closeButton = document.querySelector('.close-button');
-var filesArray = "https://res.cloudinary.com/sosnglobal/image/upload/v1651632188/sosnglobal%20website/21%20day%20bootcamp%202022/main_flyer_erv48d.jpg";
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
 
-shareButton.addEventListener('click', event => {
-  if (navigator.share && navigator.share({ files: filesArray })) {
-    navigator.share({
-      title: 'WebShare API Demo',
-      url: 'https://sosnglobal.netlify.app',
-      text: "It has pleased the Lord to permit us bring to you the first edition of SOSN-GLOBAL 21-Day Bootcamp.A program targeted at revisiting the Consecration Culture of the Saints of Old. Register on the website https://sosnglobal.netlify.app. Registration Closes on Monday,9th of May.",
-      files: filesArray
-    }).then(() => {
-      console.log('Thanks for sharing!');
-    })
-    .catch(console.error);
-  } else {
-    shareDialog.classList.add('is-open');
-  }
-});
+   /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.innerText);
 
-closeButton.addEventListener('click', event => {
-  shareDialog.classList.remove('is-open');
-});
+  /* Alert the copied text */
+  alert("Text Copied to clipboard");
+}
